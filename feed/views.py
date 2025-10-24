53b87398895c547276fd3ee06244871cf8d2502b
Arun Dhanjal <arundhanjal@hotmail.com>
2025-10-24 15:05:55 +0100

    Create feed views for feed and create_post

--- feed/views.py
+++ feed/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-def home_page_feed(request):
-    return HttpResponse("This will be the home page feed.")
+
+def feed(request):
+    posts = Post.objects.filter(is_approved=True)
+    return render(request, "feed/feed.html", {"posts": posts})
+
+
+@login_required
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect(home_page_feed)
+    else:
+        form = PostForm()
+    return render(request, "feed/create_post.html", {"form": form})
