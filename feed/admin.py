1eb258196ded41fab17f5698a32649ffa116f8fb
Arun Dhanjal <arundhanjal@hotmail.com>
2025-10-24 16:40:48 +0100

    Set up post admin functionality

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("author", "created_at", "is_approved")
+    list_filter = ("is_approved",)
+    actions = ["approve_posts"]
+
+    def approve_posts(self, request, queryset):
+        queryset.update(is_approved=True)
+    approve_posts.short_description = "Approve selected posts"
