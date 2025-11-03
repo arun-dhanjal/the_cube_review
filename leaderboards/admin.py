10f13cb83042300adf4188b250ecd6ed548f39e7
Arun Dhanjal <arundhanjal@hotmail.com>
2025-11-03 20:24:43 +0000

    Add admin view for TimeSubmissions model

--- leaderboards/admin.py
+++ leaderboards/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import TimeSubmission
 
 # Register your models here.
+
+
+@admin.register(TimeSubmission)
+class TimeSubmission(admin.ModelAdmin):
+    list_display = ("__str__",)
